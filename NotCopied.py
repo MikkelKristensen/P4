ba9262e2ca111443ed70c6ee2ca005b0f47ce9a5
Bobthewild <arosba18@student.aau.dk>
2020-04-24 10:15:18 +0200

    Update NotCopied.py

--- NotCopied.py
+++ NotCopied.py
@@ -2,28 +2,33 @@ import pyaudio
 import time
 import wave
 import numpy as np
+from numpy import float32
 import Lowpass
-
+import matplotlib.pyplot as plt
+import sounddevice as SD
 
 CHUNK = 4096
 bufferforUnpack = CHUNK*2
+
 ChannelIN = 1
 rate = 44100
 
+cutoff = 4000
 
 
 
 p = pyaudio.PyAudio()
 
-steamIN = p.open(format=pyaudio.paFloat32,
+steamIN = p.open(format=pyaudio.paInt16,
                  channels=ChannelIN,
                  rate=rate,
                  input=True,
+                 frames_per_buffer=CHUNK,
                 )
 
-#frames_per_buffer=CHUNK
 
-steamOUT = p.open(format=pyaudio.paFloat32,
+
+steamOUT = p.open(format=pyaudio.paInt16,
                   channels=1,
                   rate=rate,
                   output=True)
@@ -32,7 +37,7 @@ steamIN.start_stream()
 
 def liveRecording(steamIn, num):
     data = steamIn.read(CHUNK, exception_on_overflow=False)
-    waveData = wave.struct.unpack("%dh"%(bufferforUnpack), data)
+    waveData = wave.struct.unpack("%dh"%(CHUNK), data)
     npArrayData = np.array(waveData)
     samples = npArrayData.astype(np.int16).tostring()
 
@@ -46,12 +51,15 @@ def liveRecording(steamIn, num):
         return data
 
 
+
 while steamIN.is_active():
     Rec = liveRecording(steamIN, 1)
-    lowFreq = Lowpass.lowpass(Rec, CHUNK, 2048)
-    bandpass = Lowpass.highpass(lowFreq, CHUNK, -1024)
+
+    lowFreq = Lowpass.lowpass(Rec, CHUNK, cutoff)
+    bandpass = Lowpass.highpass(lowFreq, CHUNK, -cutoff)
+
     npA = np.array(bandpass)
-    playable = npA.astype(np.int16).tostring()
+    playable = npA.real.astype(np.int16).tostring()
     steamOUT.write(playable)
 
 
